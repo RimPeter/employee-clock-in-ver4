262fe2f0f6113dd4f3cef1ff90e4ab29a24976e1
RimPeter <hull@localappliancerentals.co.uk>
2024-06-26 20:11:58 +0100

    Change urls for employeeapp

--- clockin/clockin/urls.py
+++ clockin/clockin/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-from employeeapp import views as employeeapp_views
+from employeeapp import views as index_views
 
 urlpatterns = [
-    path('', employeeapp_views.index, name='index'),
+    path('', index_views.index, name='index'),
     path('admin/', admin.site.urls),
     
 ]
